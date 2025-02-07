3c63aa0afa8cf7044eb9a1fb93975a33dd5bb64f
applegithubuser <appleact09@gmail.com>
2025-02-08 00:51:33 +0600

    add netlify live  link

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,13 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCwkgKRwd1bKp1ihAm9oVLTWYOvdzphaKo",
-    authDomain: "visa-57165.firebaseapp.com",
-    projectId: "visa-57165",
-    storageBucket: "visa-57165.firebasestorage.app",
-    messagingSenderId: "752871725539",
-    appId: "1:752871725539:web:207c8a9cdfa1923e7368d6"
+  apiKey: "AIzaSyCQibVLbVPSffF05Ub9Nn8IUpl-QIL0Rqg",
+  authDomain: "final-assignment-b8f54.firebaseapp.com",
+  projectId: "final-assignment-b8f54",
+  storageBucket: "final-assignment-b8f54.firebasestorage.app",
+  messagingSenderId: "168420939881",
+  appId: "1:168420939881:web:f2c7bfe9ddd1b44664eaf8",
+  measurementId: "G-4E0ZCBDS10"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export default app;
